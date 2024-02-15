f5e3535fe60e88ae8ca8842387284809f6378683
Sh4un221 <127805282+Sh4un221@users.noreply.github.com>
2024-02-15 15:03:53 -0500

    feat: :construction: Se esta trabajando en el CRUD de actores

--- modules/actores.py
+++ modules/actores.py
@@ -4,3 +4,13 @@ from .variables import getAllActores,saveActores,actoresList
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 fileP = os.path.join(current_directory, '..', 'storage', 'peliculasData.json')
+
+if os.path.exists(fileP) and os.path.getsize(fileP) > 0:
+    with open(fileP, 'r') as file:
+        actoresList.extend(json.load(file))
+def crearActores():
+    actor={
+        "id"(input("Ingrese ID del actor: ")),
+        "nombre"(input("Ingrese nombre del actor: "))
+    }
+    saveActores(actor)
